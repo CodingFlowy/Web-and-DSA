24712846ac5b61049a497e1e0676ecfe8d25a835
CodingFlowy <146538513+CodingFlowy@users.noreply.github.com>
2025-09-12 06:32:21 +0530

    Add OOP and exception handling examples
    Expanded FirstDay.py with examples of lists, tuples, OOP concepts (classes, inheritance, encapsulation), and added a new SecondDay.py file covering function overloading, method overriding, exception handling, custom errors, and data structures. These additions provide foundational Python programming concepts for learning and reference.

--- PythonProgramming/FirstDay.py
+++ PythonProgramming/FirstDay.py
@@ -1,13 +1,83 @@
 print("Helloworld")
 my_name = "Rahul Singh"
+'''
 def my_function():
     print("Hi " + my_name[0:5])
 my_function()
 
-age = 21
-txt = " my age is {}"
-print(txt.format(age))
+# age = 21
+# txt = " my age is {}"
+# print(txt.format(age))
 #string.upper() , string.lower()
 
 # for i in my_name:
 #     print(i)
+
+#---------------------List----------
+items = ["seb","kela","aaloo","tamatar"]
+notItems = ["air","counting"]
+print(items)
+print(items+notItems)
+print(len(items))
+
+#-- tuples - An ordered and unchangeable group of things is referred to as a tuple
+tup1 = ('sabji',"aaloo","tamatar");
+tup2 = (1,5,9);
+print(tup1)
+print(tup1+tup2)
+print(tup2*2)
+print(5 in tup2)
+for item in tup1:
+    print(item)
+
+#---OOP concepts---------
+class Testing:
+    
+
+    def __init__(self,name):   #init is constructor
+        self.name = name
+attr = "test1"
+#---> A function connected to an object is the method
+
+
+obj = Testing(attr)
+print(obj.name)
+'''
+#---------------inheritance-------------------\
+'''
+class Person:  #parent class
+    def __init__(self,name,idNumber):
+        self.name = name
+        self.idNumber = idNumber        
+    def Display(self):
+        print(self.name)
+        print(self.idNumber)
+
+class Employee(Person):
+    def __init__(self, name, idNumber,salary,post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self,name,idNumber)
+    
+a = Employee('Rahul',99,20000,'notyet')
+a.Display()   
+'''
+#------encapsulation-----
+
+class Actor:
+    def __init__(self):
+        self.m="Move"
+        self.__j="Jump"
+
+class Player(Actor):
+    def __init__(self):
+        Actor.__init__(self) #calling base class constructor
+        print("calling private member of base class: ")
+        print(self.m)
+
+obj1 = Actor()
+print(obj1.m)
+
+obj2 = Player()
+
+
---
+++ PythonProgramming/SecondDay.py
@@ -0,0 +1,106 @@
+#-------------Function overloading ------------
+'''
+class sumClass:
+    #def sum(self,a,b):
+     #   print("First Method: " ,a+b)
+    def sum(self,a=None,b=None,c=None):
+        if a != None and b==None or c==None:
+            print("provide more numbers")
+        else:
+            print("First Method:",a+b+c)
+
+obj = sumClass()    
+obj.sum(19,8,77)
+obj.sum(18,20)
+obj.sum(15)
+'''
+#-------------method Overriding-----------------
+'''
+class Parent:
+    def __init__(self):
+        self.value = "Parent object"
+
+    def show(self):
+        print(self.value)
+
+class Child:
+    def __init__(self):
+        self.value = "Child Object"
+
+    def show(self):
+        print(self.value)
+
+ob1 = Parent()
+ob2 = Child()
+
+ob1.show()
+ob2.show()
+'''
+#----------------exception Handling-------------
+'''
+import sys
+r_list = ['a',0,2]
+
+for entry in r_list:
+    r = 1/int(entry)
+    try:
+        print("the entry is : ",entry)
+        
+        break
+    except:
+        print("Opps",sys.exc_info()[0],"Occurred.")
+        print("Next Entry\\n")
+        print("the reciprocal of ",entry,"is",r)
+        
+try: 
+   # do something 
+   pass 
+except ValueError: 
+   # handle ValueError exception 
+   pass 
+except (TypeError, ZeroDivisionError): 
+   # handle multiple exceptions 
+   # TypeError and ZeroDivisionError 
+   pass 
+except: 
+   # handle all other exceptions 
+   pass
+
+'''
+"""
+try:
+    num = int(input("Enter a number: "))
+    assert num % 2 == 0
+except:
+    print("not an even number")
+else:
+    reciprocal = 1/num
+    print(reciprocal)
+    """
+#Custom Error Syntax
+'''
+>>> class CustomError(Exception): 
+...     
+... 
+pass 
+>>> raise CustomError 
+Traceback (most recent call last): 
+... 
+__main__.CustomError 
+>>> raise CustomError("An error occurred") 
+Traceback (most recent call last): 
+... 
+__main__.CustomError: An error occurred
+'''
+#-------------
+# creating a list containing elements belonging to different data types 
+
+'''
+sample_list = [1,"ABC",['k','j']] 
+print(sample_list)
+import array 
+sample_array = array.array('i', [4, 5, 6]) 
+# accessing elements of array 
+for i in sample_array: 
+ print(i)
+ '''
